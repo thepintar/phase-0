f01a8d813209e2669389665bffa0e5c2da0996db
Michael Pintar <thepintar@gmail.com>
2016-01-19 08:41:24 -0600

    begin group project

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,5 @@
+// User Story:
+// As a user, I want to be able to take a group of numbers and do three things to it:
+// I want to be able to take the sum of all the numbers, I want to be able to find the mean of all the numbers, and I want to be able to find
+// the median of all the numbers. I want to be able to do all of these things individually with functions called sum, mean and median.
+// Keep in mind that my group of numbers might contain an odd number of numbers or an even number of numbers. This will impact the median function.
